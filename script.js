ecfe99c9814328b7c2f1efa44568ee16bc6ec910
Aman Bilaiya <2018csb1069@iitrpr.ac.in>
2020-07-02 12:08:42 +0530

    BS4, winner log added

--- script.js
+++ script.js
@@ -83,6 +83,41 @@ board.addEventListener('click',e=>{
         }
 
         // console.log(board_state);
+    var b1 = board_state[0];
+    var b2 = board_state[1];
+    var b3 = board_state[2];
+    var b4 = board_state[3];
+    var b5 = board_state[4];
+    var b6 = board_state[5];
+    var b7 = board_state[6];
+    var b8 = board_state[7];
+    var b9 = board_state[8];
+
+    if (((b1=="X") || (b1=="O")) && ((b1 == b2) && (b2 == b3))) {
+        console.log(b1 + ' WON');
+    }
+    else if (((b1=="X") || (b1=="O")) && ((b1 == b4) && (b4 == b7))){
+        console.log(b1 + ' WON');
+    }
+    else if (((b9=="X") || (b9=="O")) && ((b9 == b8) && (b8 == b7))){
+        console.log(b9 + ' WON');
+    }
+    else if (((b9=="X") || (b9=="O")) && ((b9 == b6) && (b6 == b3))){
+        console.log(b9 + ' WON');
+    }
+    else if (((b4=="X") || (b4=="O")) && ((b4 == b5) && (b5 == b6))){
+        console.log(b4 + ' WON');
+    }
+    else if (((b2=="X") || (b2=="O")) && ((b2 == b5) && (b5 == b8))){
+        console.log(b2 + ' WON');
+    }
+    else if (((b1=="X") || (b1=="O")) && ((b1 == b5) && (b5== b9))){
+        console.log(b1 + ' WON');
+    }
+    else if (((b7=="X") || (b7=="O")) && ((b7 == b5) && (b5 == b3))){
+        console.log(b7 + ' WON');
+    }
+
     }
 
 });
