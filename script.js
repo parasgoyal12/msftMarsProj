38524b8ba1111b249cc694080deb5cc27e369344
Paras Goyal <parasgoyal12@gmail.com>
2020-07-04 10:54:03 +0530

    Updates Board

--- script.js
+++ script.js
@@ -119,6 +119,7 @@ starting_player.addEventListener('click',e=>{
     turn=Number(e.target.getAttribute('data-value'));
     }
     clear_board();
+    
 });
 
 const clear_board=()=>{
@@ -126,68 +127,41 @@ const clear_board=()=>{
     elements.forEach(item=>{
         item.innerText=' ';
     });
+    board_state=new Board();
 };
 
 document.querySelector('#newgame').addEventListener('click',clear_board);
 
-const check_board= state=>{
-    //Complete Me
-}
 
-board_state=Array(9).fill(" ");
+board_state=new Board();
 board.addEventListener('click',e=>{
     if(e.target.tagName==='TD'){
-        if(e.target.innerText==='X' || e.target.innerText==='O'){
-            console.log('error!Invalid Move')
-        }
-        else{
-            if(turn===1){
-                e.target.innerText='X';
-                board_state[Number(e.target.id)]='X';
-                turn=0;
+        // if(e.target.innerText==='X' || e.target.innerText==='O'){
+        //     console.log('error!Invalid Move')
+        // }
+        // else{
+            // console.log('Hello');
+        if(turn===1){
+            if(!board_state.isTerminal()&&board_state.insert('X',e.target.id)){
+            e.target.innerText='X';
+            if(board_state.isTerminal())console.log(`X Wins!!`);
+            turn=0;
             }
             else{
-                e.target.innerText='O';
-                board_state[Number(e.target.id)]='O';
-                turn=1;
+                console.log("Error! Invalid Move")
             }
         }
-
-        // console.log(board_state);
-    var b1 = board_state[0];
-    var b2 = board_state[1];
-    var b3 = board_state[2];
-    var b4 = board_state[3];
-    var b5 = board_state[4];
-    var b6 = board_state[5];
-    var b7 = board_state[6];
-    var b8 = board_state[7];
-    var b9 = board_state[8];
-
-    if (((b1=="X") || (b1=="O")) && ((b1 == b2) && (b2 == b3))) {
-        console.log(b1 + ' WON');
-    }
-    else if (((b1=="X") || (b1=="O")) && ((b1 == b4) && (b4 == b7))){
-        console.log(b1 + ' WON');
-    }
-    else if (((b9=="X") || (b9=="O")) && ((b9 == b8) && (b8 == b7))){
-        console.log(b9 + ' WON');
-    }
-    else if (((b9=="X") || (b9=="O")) && ((b9 == b6) && (b6 == b3))){
-        console.log(b9 + ' WON');
-    }
-    else if (((b4=="X") || (b4=="O")) && ((b4 == b5) && (b5 == b6))){
-        console.log(b4 + ' WON');
-    }
-    else if (((b2=="X") || (b2=="O")) && ((b2 == b5) && (b5 == b8))){
-        console.log(b2 + ' WON');
-    }
-    else if (((b1=="X") || (b1=="O")) && ((b1 == b5) && (b5== b9))){
-        console.log(b1 + ' WON');
-    }
-    else if (((b7=="X") || (b7=="O")) && ((b7 == b5) && (b5 == b3))){
-        console.log(b7 + ' WON');
-    }
+        else{
+            if(!board_state.isTerminal()&&board_state.insert('O',e.target.id)){
+            e.target.innerText='O';
+            if(board_state.isTerminal())console.log(`O Wins!!`);
+            turn=1;
+            }
+            else{
+                console.log("Error");
+            }
+        }
+        
 
     }
 
